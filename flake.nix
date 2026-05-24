5a5d764845bde8435d3f1bf3010d462a43c6c749
Rodman <rodman@stuhlmuller.net>
2026-05-23 22:24:30 -0700

    feat: new homelab

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Development shell for operating the homelab";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              awscli2
+              kubectl
+              opentofu
+              talosctl
+              terragrunt
+            ];
+          };
+        }
+      );
+    };
+}
