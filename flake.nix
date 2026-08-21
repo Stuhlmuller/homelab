bf387040ca7188de81df4cf9b34944f72d63a5f8
Rodman <rodman@stuhlmuller.net>
2026-08-21 10:13:40 -0700

    fix(devshell): package octelium cli

--- flake.nix
+++ flake.nix
@@ -24,6 +24,46 @@
           pkgs = import nixpkgs {
             inherit system;
           };
+          octeliumPackage =
+            let
+              version = "0.39.0";
+              sources = {
+                x86_64-linux = {
+                  platform = "linux-amd64";
+                  hash = "sha256-FzHbR38Oy9Puug5fud8fqxr8f78a76/Qb9ZqN91bu0w=";
+                };
+                aarch64-linux = {
+                  platform = "linux-arm64";
+                  hash = "sha256-VlzV35zR4UqMGtDHhJUhxtyLoSy+ixTMY5W+Bioi+k0=";
+                };
+                x86_64-darwin = {
+                  platform = "darwin-amd64";
+                  hash = "sha256-5/qUb/2iKTboIciG6PvEfXBmDdXL/yB7/I82eR9nXeM=";
+                };
+                aarch64-darwin = {
+                  platform = "darwin-arm64";
+                  hash = "sha256-paSdiKZKqDPffwhdZ2y/TT/J/lNYfmBoGYYKqVzBTwI=";
+                };
+              };
+              source =
+                sources.${system}
+                  or (throw "Unsupported Octelium platform for ${system}");
+            in
+            pkgs.stdenv.mkDerivation {
+              pname = "octelium";
+              inherit version;
+
+              src = pkgs.fetchurl {
+                url = "https://github.com/octelium/octelium/releases/download/v${version}/octelium-${source.platform}.tar.gz";
+                inherit (source) hash;
+              };
+
+              installPhase = ''
+                runHook preInstall
+                install -Dm755 octelium "$out/bin/octelium"
+                runHook postInstall
+              '';
+            };
           basePackages = with pkgs; [
             actionlint
             age
@@ -49,7 +89,6 @@
             kustomize
             miniupnpc
             nixVersions.latest
-            octelium
             opentofu
             openssh
             pre-commit
@@ -62,6 +101,7 @@
             unzip
             yamllint
             yq-go
+            octeliumPackage
           ];
           checkovPackages = pkgs.lib.optionals (system != "x86_64-darwin") [
             pkgs.checkov
